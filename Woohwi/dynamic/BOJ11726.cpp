78110b783db5e47c98a5380779df0caff7c71b5e
rladngnl <rladngnl@gmail.com>
2024-10-15 10:19:59 +0900

    타일링

---
+++ Woohwi/dynamic/BOJ11726.cpp
@@ -0,0 +1,28 @@
+/*
+https://www.acmicpc.net/problem/11726
+2*N 타일링
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int table[n];
+
+    table[1] = 1;
+    table[2] = 2;
+
+    for(int i = 3; i <= n; i++){
+        table[i] = (table[i-1]+table[i-2]) % 1007;
+    }
+
+    cout << table[n];
+    printf("\n");
+
+
+}
